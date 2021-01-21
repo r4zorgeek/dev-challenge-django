53f7f78cb0641d1b252aad5d6f96c07a9878fdd0
r4zorgeek <abhishekmane747@gmail.com>
2021-01-21 14:44:06 +0530

    api:interest_data implemented

--- interest_calculator/views.py
+++ interest_calculator/views.py
@@ -1,9 +1,22 @@
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
+import json
 
 @require_POST
 @csrf_exempt
 def interest_data(request):
-    # Just an example! Should be replaced with real calculated data
-    return JsonResponse({'result': 1000})
+    request_json = json.loads(request.body.decode())
+    savings_amount = int(request_json['savings-amount'])
+    monthly_deposit = int(request_json['monthly-deposit'])
+    interest_rate = round(int(request_json['interest-rate']) / 100, 2)
+    compound_interest_yearly = []
+    
+    for year in range(0, 51):
+        r = (((1 + interest_rate) ** (year)))
+        compound_interest_yearly.append(round((monthly_deposit * ((r-1)/interest_rate)) + (savings_amount * r), 2))
+        # compound_interest_yearly[year] = savings_amount
+        
+        response_result = {'compound_data_yearly': compound_interest_yearly}
+        
+    return JsonResponse(response_result)
